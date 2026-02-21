6a3822953c08a390924f7aca6befe7d0d2afb13f
Snesnopic <snesnopic@gmail.com>
2026-02-21 16:12:15 +0100

    Fix tag reinsertion on opus and vorbis files

--- libchisel/src/utils/audio_metadata_util.cpp
+++ libchisel/src/utils/audio_metadata_util.cpp
@@ -610,21 +610,20 @@ bool AudioMetadataUtil::rebuildCovers(const std::filesystem::path &input_path,
             TagLib::ByteVector data = readFileToByteVector(info.temp_file_path);
             if (data.isEmpty()) continue;
 
-            // ogg uses the same picture format as flac but doesn't own pointers
-            TagLib::FLAC::Picture pic;
-            pic.setMimeType(info.mime_type);
-            pic.setDescription(info.description);
-            pic.setType(static_cast<TagLib::FLAC::Picture::Type>(info.picture_type));
-            pic.setData(data);
+            auto *pic = new TagLib::FLAC::Picture;
+            pic->setMimeType(info.mime_type);
+            pic->setDescription(info.description);
+            pic->setType(static_cast<TagLib::FLAC::Picture::Type>(info.picture_type));
+            pic->setData(data);
 
             int w=0, h=0, d=0, c=0;
             computeImageProps(info.temp_file_path, info.mime_type, w, h, d, c);
-            if (w > 0) pic.setWidth(w);
-            if (h > 0) pic.setHeight(h);
-            if (d > 0) pic.setColorDepth(d);
-            if (c > 0) pic.setNumColors(c);
+            if (w > 0) pic->setWidth(w);
+            if (h > 0) pic->setHeight(h);
+            if (d > 0) pic->setColorDepth(d);
+            if (c > 0) pic->setNumColors(c);
 
-            xc->addPicture(&pic); // taglib base64-encodes this automatically
+            xc->addPicture(pic);
         }
         return oggVorbis->save();
     }
@@ -640,20 +639,20 @@ bool AudioMetadataUtil::rebuildCovers(const std::filesystem::path &input_path,
             TagLib::ByteVector data = readFileToByteVector(info.temp_file_path);
             if (data.isEmpty()) continue;
 
-            TagLib::FLAC::Picture pic;
-            pic.setMimeType(info.mime_type);
-            pic.setDescription(info.description);
-            pic.setType(static_cast<TagLib::FLAC::Picture::Type>(info.picture_type));
-            pic.setData(data);
+            auto *pic = new TagLib::FLAC::Picture;
+            pic->setMimeType(info.mime_type);
+            pic->setDescription(info.description);
+            pic->setType(static_cast<TagLib::FLAC::Picture::Type>(info.picture_type));
+            pic->setData(data);
 
             int w=0, h=0, d=0, c=0;
             computeImageProps(info.temp_file_path, info.mime_type, w, h, d, c);
-            if (w > 0) pic.setWidth(w);
-            if (h > 0) pic.setHeight(h);
-            if (d > 0) pic.setColorDepth(d);
-            if (c > 0) pic.setNumColors(c);
+            if (w > 0) pic->setWidth(w);
+            if (h > 0) pic->setHeight(h);
+            if (d > 0) pic->setColorDepth(d);
+            if (c > 0) pic->setNumColors(c);
 
-            xc->addPicture(&pic);
+            xc->addPicture(pic);
         }
         return oggOpus->save();
     }
