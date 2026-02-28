b18168fcb09dff2e66746d9b183bccae11d5e196
Snesnopic <snesnopic@gmail.com>
2026-02-28 14:57:49 +0100

    Version bump, 1.2.0

--- chisel_cli/src/cli/cli_parser.cpp
+++ chisel_cli/src/cli/cli_parser.cpp
@@ -12,7 +12,7 @@
 void setup_cli_parser(CLI::App& app, Settings& settings) {
     // setup standard help and version flags
     app.set_help_flag("-h,--help", "Show this help message and exit.");
-    app.set_version_flag("--version", "1.1.0");
+    app.set_version_flag("--version", "1.2.0");
 
     // --- Flags (booleans) ---
     app.add_flag("--no-meta", [&](const int count) {
