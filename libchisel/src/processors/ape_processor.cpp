424e48256b428c0c8b447fadbf8699e7573508ab
Snesnopic <snesnopic@gmail.com>
2026-03-15 14:00:57 +0100

    Revert attempted freeBSD fix

--- libchisel/src/processors/ape_processor.cpp
+++ libchisel/src/processors/ape_processor.cpp
@@ -80,14 +80,9 @@ void ApeProcessor::recompress(const std::filesystem::path& input,
     }
 
     int err = 0;
-#ifdef __FreeBSD__
-    APE::IAPEDecompress *pDecompress = CreateIAPEDecompress(
-        reinterpret_cast<const APE::str_utfn*>(input.c_str()),
-        &err, true, true, false);
-#else
+
     APE::IAPEDecompress *pDecompress = CreateIAPEDecompress(
         input.wstring().c_str(), &err, true, true, false);
-#endif
     if (pDecompress == nullptr || err != ERROR_SUCCESS) {
         delete pDecompress;
         throw std::runtime_error("ApeProcessor: cannot create APE decompress (err: " + std::to_string(err) + ")");
