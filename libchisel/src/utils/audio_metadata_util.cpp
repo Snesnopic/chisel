bedcf65d19b07a421e6e57a55b919f8e1f4bbc0e
Snesnopic <snesnopic@gmail.com>
2026-04-12 17:58:58 +0200

    Revert "Fix memory leak"
    This reverts commit af9a73bea405a5b0617a6b7c6798e396fa052798.

--- libchisel/src/utils/audio_metadata_util.cpp
+++ libchisel/src/utils/audio_metadata_util.cpp
@@ -728,12 +728,13 @@ bool AudioMetadataUtil::rebuildCovers(const std::filesystem::path &input_path,
             TagLib::ByteVector data = readFileToByteVector(info.temp_file_path);
             if (data.isEmpty()) continue;
 
-            auto pic = std::make_unique<TagLib::FLAC::Picture>();
+            auto *pic = new TagLib::FLAC::Picture;
             pic->setMimeType(info.mime_type);
             pic->setDescription(info.description);
             pic->setType(static_cast<TagLib::FLAC::Picture::Type>(info.picture_type));
             pic->setData(data);
 
+            // compute technical fields from the *optimized* image data
             int w=0, h=0, d=0, c=0;
             computeImageProps(info.temp_file_path, info.mime_type, w, h, d, c);
             if (w > 0) pic->setWidth(w);
@@ -741,7 +742,7 @@ bool AudioMetadataUtil::rebuildCovers(const std::filesystem::path &input_path,
             if (d > 0) pic->setColorDepth(d);
             if (c > 0) pic->setNumColors(c);
 
-            flacFile->addPicture(pic.get());
+            flacFile->addPicture(pic);
         }
         return flacFile->save();
     }
@@ -817,7 +818,7 @@ bool AudioMetadataUtil::rebuildCovers(const std::filesystem::path &input_path,
             TagLib::ByteVector data = readFileToByteVector(info.temp_file_path);
             if (data.isEmpty()) continue;
 
-            auto pic = std::make_unique<TagLib::FLAC::Picture>();
+            auto *pic = new TagLib::FLAC::Picture;
             pic->setMimeType(info.mime_type);
             pic->setDescription(info.description);
             pic->setType(static_cast<TagLib::FLAC::Picture::Type>(info.picture_type));
@@ -833,7 +834,7 @@ bool AudioMetadataUtil::rebuildCovers(const std::filesystem::path &input_path,
             if (d > 0) pic->setColorDepth(d);
             if (c > 0) pic->setNumColors(c);
 
-            xc->addPicture(pic.get());
+            xc->addPicture(pic);
         }
         return oggVorbis->save();
     }
@@ -849,7 +850,7 @@ bool AudioMetadataUtil::rebuildCovers(const std::filesystem::path &input_path,
             TagLib::ByteVector data = readFileToByteVector(info.temp_file_path);
             if (data.isEmpty()) continue;
 
-            auto pic = std::make_unique<TagLib::FLAC::Picture>();
+            auto *pic = new TagLib::FLAC::Picture;
             pic->setMimeType(info.mime_type);
             pic->setDescription(info.description);
             pic->setType(static_cast<TagLib::FLAC::Picture::Type>(info.picture_type));
@@ -862,7 +863,7 @@ bool AudioMetadataUtil::rebuildCovers(const std::filesystem::path &input_path,
             if (d > 0) pic->setColorDepth(d);
             if (c > 0) pic->setNumColors(c);
 
-            xc->addPicture(pic.get());
+            xc->addPicture(pic);
         }
         return oggOpus->save();
     }
