d2d96c2d2f481cf713b79bbe2653c0006924539a
Snesnopic <snesnopic@gmail.com>
2026-09-24 10:57:20 +0200

    Support the .vcard extension

--- libchisel/include/vcf_processor.hpp
+++ libchisel/include/vcf_processor.hpp
@@ -39,7 +39,7 @@ namespace chisel {
         }
 
         [[nodiscard]] std::span<const std::string_view> get_supported_extensions() const noexcept override {
-            static constexpr std::array<std::string_view, 1> kExts = { ".vcf" };
+            static constexpr std::array<std::string_view, 2> kExts = { ".vcf", ".vcard" };
             return {kExts.data(), kExts.size()};
         }
 
